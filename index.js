a13eaebc2e3e9f11e9f884b1bb4cd7d9f99de3b0
mubashir27 <mubashirsiddiqui27@gmail.com>
2024-03-13 19:50:33 +0500

    without swagger

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
-
-// Define Swagger specification options
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Chat app API',
-      version: '0.0.1',
-      description: 'API documentation for Chat app',
-    },
-    servers: [{
-      url: 'http://localhost:3000/', // Adjust the URL according to your deployment environment
-    }],
-  },
-  // Path to the API specs
-  apis: ['./index.js'], // Point to the file containing your API endpoints
-};
-
-// Initialize Swagger-jsdoc
-const specs = swaggerJsdoc(options);
-
-// Serve Swagger UI at the endpoint /api-docs
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // Define your API routes in index.js
 app.get("/", (req, res, next) => {
@@ -38,23 +13,6 @@ app.get("/", (req, res, next) => {
 app.get("/path", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from path!",
-  });
-});
-
-// Define a new API endpoint and document it with Swagger
-/**
- * @swagger
- * /api/greet:
- *   get:
- *     summary: Greet the user
- *     description: Returns a greeting message.
- *     responses:
- *       200:
- *         description: A greeting message.
- */
-app.get("/api/greet", (req, res, next) => {
-  return res.status(203).json({
-    message: "Hello, welcome API!",
   });
 });
 
